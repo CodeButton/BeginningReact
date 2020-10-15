9e904e9283a7c2c107a7591943ca8d851b121872
CodeButton <jsc9139@gmail.com>
2020-10-15 03:18:40 +0000

    Arrange Componets files.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TOC from './Components/TOC.js';
+import Content from './Components/Content.js';
+import Subject from './Components/Subject.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Subject title = "WEB" subtitle = "world wide web!"></Subject>
+        <Subject title = "React" subtitle = "Studyung React!"></Subject>    
         Hello, React!!
+        <TOC></TOC>
+        <Content title = "HTML" desc = "HTML is HyperText Markup Language"></Content>
+        <Content title = "CSS" desc = "CSS is Cascading Style Sheets"></Content>
       </div>
     );
   }
